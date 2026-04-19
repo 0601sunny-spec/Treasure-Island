a80466a02390c2a98b6a83ac3d1d5c05468e29cd
qkrwndnjs2640@naver.com <qkrwndnjs2640@naver.com>
2026-04-20 01:02:39 +0900

    hi!!!!!!!!

--- 2-1.js
+++ 2-1.js
@@ -1,69 +1,52 @@
-window.onload = function() {
-    // 1. 수원대학교 건물별 정밀 좌표 데이터
+window.onload = function () {
+    // 1. 수원대학교 건물별 정밀 좌표 데이터 (고정)
     const placeList = [
-        { id: 1, name: '도서관', img: 'library', lat: 37.208633, lng: 126.975936 },
-        { id: 2, name: '학생회관', img: 'student', lat: 37.209134, lng: 126.977890 },
-        { id: 3, name: '종합관', img: 'total', lat: 37.209083, lng: 126.978658 }, //여기까지 됨
-        { id: 4, name: '아마랜스 홀', img: 'amaranth', lat: 37.2112, lng: 126.9778 },
-        { id: 5, name: '정문', img: 'front', lat: 37.2100, lng: 126.9752 },
-        { id: 6, name: '미래혁신관', img: 'future', lat: 37.2098, lng: 126.9765 },
-        { id: 7, name: '인문사회융합대학', img: 'human', lat: 37.2106, lng: 126.9785 },
-        { id: 8, name: 'ICT융합대학', img: 'ict', lat: 37.2128, lng: 126.9795 },
-        { id: 9, name: '야구장', img: 'baseball', lat: 37.2148, lng: 126.9785 },
-        { id: 10, name: '대운동장', img: 'ground', lat: 37.2158, lng: 126.9765 }
+        { id: 1, name: '도서관', img: 'library', lat: 37.208633, lng: 126.975936, mission_type: 'quiz' },
+        { id: 2, name: '학생회관', img: 'student', lat: 37.209134, lng: 126.977890, mission_type: 'photo' },
+        { id: 3, name: '종합관', img: 'total', lat: 37.209083, lng: 126.978658, mission_type: 'quiz' },
+        { id: 4, name: '아마랜스 홀', img: 'amaranth', lat: 37.2112, lng: 126.9778, mission_type: 'quiz' },
+        { id: 5, name: '정문', img: 'front', lat: 37.2100, lng: 126.9752, mission_type: 'photo' },
+        { id: 6, name: '미래혁신관', img: 'future', lat: 37.2098, lng: 126.9765, mission_type: 'quiz' },
+        { id: 7, name: '인문사회융합대학', img: 'human', lat: 37.2106, lng: 126.9785, mission_type: 'quiz' },
+        { id: 8, name: 'ICT융합대학', img: 'ict', lat: 37.2101, lng: 126.9810, mission_type: 'photo' }
     ];
 
-    // 2. 카카오 지도 초기화
-    kakao.maps.load(() => {
-        const container = document.getElementById('kakao-map');
-        const options = {
-            center: new kakao.maps.LatLng(37.2125, 126.9765), // 학교 중심
-            level: 4 // 건물들이 다 보이게 축척 조정
-        };
-        const map = new kakao.maps.Map(container, options);
+    const API_URL = "https://dopamine-treasure-backend-production.up.railway.app";
 
-        // 3. 마커 생성 루프
-        placeList.forEach(place => {
-            const markerPosition = new kakao.maps.LatLng(place.lat, place.lng);
-            
-            // 마커 생성
-            const marker = new kakao.maps.Marker({
-                position: markerPosition,
-                map: map
-            });
+    // 카카오맵 초기화
+    var mapContainer = document.getElementById('kakao-map');
+    var mapOption = { center: new kakao.maps.LatLng(33.450701, 126.570667), level: 3 };
+    var map = new kakao.maps.Map(mapContainer, mapOption);
 
-            // 마커 위에 이름 표시 (인포윈도우)
-            const iwContent = `<div style="padding:5px; font-size:12px; text-align:center;">${place.name}</div>`;
-            const infowindow = new kakao.maps.InfoWindow({
-                content: iwContent
+    // 실시간 보물 개수 가져오기 (필요 시)
+    fetch(`${API_URL}/locations`)
+        .then(res => res.json())
+        .then(data => {
+            const counts = data.locations;
+            placeList.forEach(p => {
+                const serverData = counts.find(s => s.id === p.id);
+                if (serverData) p.treasure_count = serverData.treasure_count;
             });
-
-            // 마커에 마우스 올리면 이름 보이기
-            kakao.maps.event.addListener(marker, 'mouseover', function() {
-                infowindow.open(map, marker);
-            });
-            
-            // 마우스 떼면 이름 숨기기
-            kakao.maps.event.addListener(marker, 'mouseout', function() {
-                infowindow.close();
-            });
-        });
-    });
-
-    // 4. 하단 장소 그리드 렌더링
-    renderLocations(placeList);
+            renderLocations(placeList);
+        }).catch(() => renderLocations(placeList));
 };
 
-// 2-1.js 내의 renderLocations 함수 수정
 function renderLocations(places) {
     const grid = document.getElementById('locationsGrid');
     if (!grid) return;
 
-    grid.innerHTML = places.map(p => `
-        <article class="place-card">
-            <div class="place-image" style="background-image: url('./img/${p.img}.png');"></div>
-            <button class="mission-btn" onclick="location.href='2-1A.html?id=${p.id}'">미션 선택</button>
-            <div class="treasure-oval" id="count-${p.id}">0/10</div>
-        </article>
-    `).join('');
+    grid.innerHTML = places.map(p => {
+        // 미션 타입에 따른 페이지 분기
+        const targetPage = p.mission_type === 'photo' ? '2-1b.html' : '2-1a.html';
+        return `
+            <article class="place-card">
+                <div class="place-image" style="background-image: url('./img/${p.img}.png')"></div>
+                <div class="place-info">
+                    <h4 class="place-name">${p.name}</h4>
+                    <div class="treasure-oval">보물 ${p.treasure_count || 0}개 남음</div>
+                    <button class="mission-btn" onclick="location.href='${targetPage}?id=${p.id}'">미션하러 가기</button>
+                </div>
+            </article>
+        `;
+    }).join('');
 }
